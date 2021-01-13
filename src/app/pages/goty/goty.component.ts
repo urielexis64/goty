b1b9aca70b4618cf1792dc54fe25e7e0e7aa04bb
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-13 16:33:33 -0700

    getting games info through gameService

--- src/app/pages/goty/goty.component.ts
+++ src/app/pages/goty/goty.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../services/game.service';
+import { Game } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-goty',
   templateUrl: './goty.component.html',
-  styleUrls: ['./goty.component.css']
+  styleUrls: ['./goty.component.css'],
 })
 export class GotyComponent implements OnInit {
-
-  constructor() { }
+  games: Game[] = [];
+  constructor(private _gameService: GameService) {}
 
   ngOnInit(): void {
+    this._gameService.getNominated().subscribe((games) => {
+      this.games = games;
+    });
   }
-
 }
