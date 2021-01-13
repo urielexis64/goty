48c8e0dd8ecf67e20547a0796b99ee7ea8f4ec12
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-13 16:10:41 -0700

    ngCharts, Router and BrowserAnimations modules

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HorizontalBarChartComponent } from './horizontal-bar-chart/horizontal-bar-chart.component';
 import { RouterModule } from '@angular/router';
 
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { HorizontalBarChartComponent } from './horizontal-bar-chart/horizontal-bar-chart.component';
 @NgModule({
   declarations: [NavbarComponent, HorizontalBarChartComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NgxChartsModule,
+    BrowserAnimationsModule,
+  ],
   exports: [NavbarComponent, HorizontalBarChartComponent],
 })
 export class ComponentsModule {}
