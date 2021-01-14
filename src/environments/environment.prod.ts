ba343930484c8ffacc1581906dc852e5821c2ac9
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-13 20:49:14 -0700

    production and develop environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
   url: 'https://us-central1-firestore-graphic-8bacb.cloudfunctions.net',
+  firebase: {
+    apiKey: 'AIzaSyAXPmX869n2GaEQwQs_XlgW86VjNIXr3No',
+    authDomain: 'firestore-graphic-8bacb.firebaseapp.com',
+    databaseURL: 'https://firestore-graphic-8bacb-default-rtdb.firebaseio.com',
+    projectId: 'firestore-graphic-8bacb',
+    storageBucket: 'firestore-graphic-8bacb.appspot.com',
+    messagingSenderId: '731240919697',
+    appId: '1:731240919697:web:f37c020dfd7d8c2a938530',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,15 @@
 export const environment = {
   production: false,
   url: 'http://localhost:5000/firestore-graphic-8bacb/us-central1',
+  firebase: {
+    apiKey: 'AIzaSyAXPmX869n2GaEQwQs_XlgW86VjNIXr3No',
+    authDomain: 'firestore-graphic-8bacb.firebaseapp.com',
+    databaseURL: 'https://firestore-graphic-8bacb-default-rtdb.firebaseio.com',
+    projectId: 'firestore-graphic-8bacb',
+    storageBucket: 'firestore-graphic-8bacb.appspot.com',
+    messagingSenderId: '731240919697',
+    appId: '1:731240919697:web:f37c020dfd7d8c2a938530',
+  },
 };
 
 /*
