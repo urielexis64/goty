54fdb9c63e1b733b7fd9b5f854a363c4787dba4c
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-13 20:47:53 -0700

    getNominated and vote functions

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
 import { environment } from '../../environments/environment';
 import { Game } from '../interfaces/interfaces';
 
@@ -7,9 +10,23 @@ import { Game } from '../interfaces/interfaces';
   providedIn: 'root',
 })
 export class GameService {
+  games: Game[] = [];
+
   constructor(private _http: HttpClient) {}
 
   getNominated() {
-    return this._http.get<Game[]>(`${environment.url}/api/goty`);
+    if (this.games.length > 0) {
+      return of(this.games);
+    } else {
+      return this._http
+        .get<Game[]>(`${environment.url}/api/goty`)
+        .pipe(tap((response) => (this.games = response)));
+    }
+  }
+
+  vote(gameId: string) {
+    return this._http
+      .post(`${environment.url}/api/goty/${gameId}`, {})
+      .pipe(catchError((err) => of(err.error)));
   }
 }
