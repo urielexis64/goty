eac074bffb34c780640cb02f46c9d3b0c251ba34
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-13 20:52:40 -0700

    vote when click

--- src/app/pages/goty/goty.component.ts
+++ src/app/pages/goty/goty.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from '../../services/game.service';
 import { Game } from '../../interfaces/interfaces';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-goty',
@@ -15,5 +16,25 @@ export class GotyComponent implements OnInit {
     this._gameService.getNominated().subscribe((games) => {
       this.games = games;
     });
+  }
+
+  vote(game: Game) {
+    this._gameService
+      .vote(game.id)
+      .subscribe((response: { OK: boolean; message: string }) => {
+        if (response.OK) {
+          Swal.fire({
+            icon: 'success',
+            title: 'Nice!',
+            text: response.message,
+          });
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oppss...',
+            text: response.message,
+          });
+        }
+      });
   }
 }
